115d7a288e7544b701955de99c0279afecc98f6c
ZiebaTomasz <ziebatomasz3@gmail.com>
2018-09-14 19:06:04 +0200

    Initial commit

---
+++ src/main/java/com/tom/world/WorldApplication.java
@@ -0,0 +1,12 @@
+package com.tom.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorldApplication.class, args);
+  }
+}
